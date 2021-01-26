b6622adc412ccb0fe1a4df9fb65fd9f7432dcd63
dt1307 <dominiq.tudor@live.com>
2021-01-26 13:44:42 +0000

    Created the Quiz Library (QuizBackend) Added Game and Question classes

---
+++ Quiz/Program.cs
@@ -0,0 +1,31 @@
+﻿using QuizBackend;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Millionaire Quiz");
+            Game game = new Game();
+            var question = game.GetQuestion();
+            DisplayQuestion(question);
+            Console.ReadLine();
+        }
+
+        static void DisplayQuestion(Question question)
+        {
+            Console.WriteLine();
+            Console.WriteLine(question.Text);
+            Console.WriteLine(question.Answer1);
+            Console.WriteLine(question.Answer2);
+            Console.WriteLine(question.Answer3);
+            Console.WriteLine(question.Answer4);
+        }
+    }
+}
---
+++ QuizBackend/Game.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizBackend
+{
+    public class Game
+    {
+        public Question GetQuestion()
+        {
+            Question question = new Question();
+            question.ID = 1;
+            question.Text = "What is the name of brilliant scientiest Einstein?";
+            question.Answer1 = "Albert";
+            question.Answer2 = "Anthony";
+            question.Answer3 = "Aaron";
+            question.Answer4 = "Andrew";
+            return question;
+        }
+    }
+}
